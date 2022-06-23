7938fe62c6c0bf0ddb56e5c3a0baae12ef373cb9
“Aaron-Marsh” <aaronanthonymarsh@gmail.com>
2022-06-23 19:01:09 +0100

    fill in some about section details and get photos to work by installing file-loader

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,23 +1,31 @@
-import React from 'react'
+import React from 'react';
 
-import './About.css'
+import './About.css';
+import japanImage from '../../images/japan.jpg';
+import snowboardingImage from '../../images/snowboarding.jpg';
 
-const About = () => {
+// "https://free4kwallpapers.com/uploads/originals/2016/01/03/gullfoss-waterfall-near-sunset-time-in-summer-wallpaper.jpg"
+
+function About() {
     return (
         <>
-        <div className='about-section'>
-        </div>
-        <div className="about-card">
-        <h2 className="about-title">About Me</h2>
-        <div className="about-container">
-            <img src="https://free4kwallpapers.com/uploads/originals/2016/01/03/gullfoss-waterfall-near-sunset-time-in-summer-wallpaper.jpg" className="about-image" alt="Gullfoss Image" />
-            <div className="about-paragraphs">
-            <p>Hi, I'm Aaron Marsh, and I'm currently enrolled in a full stack software engineering course with futureproof.</p>
+            <div className='about-section'>
+            <div className="about-card">
+                <h2 className="about-title">About Me</h2>
+                <div className="about-container">
+                    <div className="about-paragraphs">
+                        <p>Hi, I'm Aaron Marsh, and I'm currently enrolled in a full stack software engineering course with futureproof.</p>
+                        <img src={snowboardingImage} className="about-image2" alt="Gullfoss Image" />
+                    </div>
+                    <div className="about-images">
+                        <img src={japanImage} className="about-image1" alt="Gullfoss Image" />
+                        <p className="about-paragraphs">some text</p>
+                    </div>
+                </div>
             </div>
-        </div>
-        </div>
+            </div>
         </>
-    )
+    );
 }
 
 export default About
--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -6,7 +6,7 @@ const Landing = () => {
     return (
         <>
         <div className="heading-box">
-        <h1>Aaron Marsh</h1>
+        <h1 className="fade-in-section">Aaron Marsh</h1>
         <h2 className="sub-title">Futureproof Tech Trainee</h2>
         </div>
         <img className="landing-image" src="https://free4kwallpapers.com/uploads/originals/2016/02/03/hubble-galaxy-wallpaper.jpg" alt="" />
