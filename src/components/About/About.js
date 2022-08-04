416f77b6b95e74651170782e0f63db8b60016a85
“Aaron-Marsh” <aaronanthonymarsh@gmail.com>
2022-08-04 01:42:05 +0100

    add click me p

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -26,10 +26,10 @@ function About() {
       
         let c = document.createDocumentFragment();
         for (let i=0; i<100; i++) {
-          let styles = 'transform: translate3d(' + (random(800) - 400) + 'px, ' + (random(800) - 450) + 'px, 0) rotate(' + random(360) + 'deg);\
-                        background: hsla('+random(360)+',100%,50%,1);\
+          let styles = `transform: translate3d(${random(800) - 400}px, ${random(800) - 450}px, 0) rotate(${random(360)}deg);\
+                        background: hsla(${random(360)},100%,50%,1);\
                         animation: bang 1.2s ease-out forwards;\
-                        opacity: 0';
+                        opacity: 0`;
             
           let e = document.createElement("i");
           e.style.cssText = styles.toString();
@@ -55,6 +55,7 @@ function About() {
                     </div>
                     <div>
                         <div id="confetti-div" className='confetti'></div>
+                        <p className="click-me">&#60;Click Me&#62;</p>
                         <img id="about-image1" src={japanImage} className="about-image1" alt="Aaron in Japan" onClick={confetti}/>
                         <p className="about-paragraphs">I graduated with a master's in chemical engineering from the University of Cambridge. I love working on challenging problems, collaborating with interesting people, and learning new things everyday.</p>
                         <p className="about-paragraphs">I have worked on some really fun full stack projects that you can check out below. I enjoyed working on and developing both the front end and back end of different projects.</p>
